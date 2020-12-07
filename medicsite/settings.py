3fa28df18562d3c5d578a69ef25a836d1341df40
Alex Kuimov <spoot@bk.ru>
2020-12-07 17:42:59 +0300

    change structure

--- medicsite/settings.py
+++ medicsite/settings.py
@@ -125,19 +125,7 @@ STATICFILES_DIRS = (
     '/var/www/static/',
 )
 
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
-        },
-    },
-}
+MEDIA_URL = '/media/'
+MEDIA_ROOT = (
+    os.path.join(BASE_DIR, 'media')
+)
